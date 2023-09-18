52087b982a0d6647c312063b1517537b4d797152
Vikas <vikas@coderootz.com>
2023-09-18 17:27:30 +0530

    changes related to the Api for mobile login, Department crud and logo on sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,10 +3,11 @@
     <div class="aside-logo py-8" id="kt_aside_logo">
         <!--begin::Logo-->
         <a href="{{route('home')}}" class="d-flex align-items-center">
-            <!-- <img alt="Logo" src="assets/media/images/CarsonLogo.png" class="h-75px w-90px logo" /> -->
-            <h3 class="logo">
+            <!-- <img alt="Logo" src="assets/media/images/carson_logo.png" class="h-85px w-120px logo" /> -->
+            <img alt="Logo" src="assets/media/images/Carson-logo.png" class="h-90px w-120px logo" />
+            <!-- <h3 class="logo">
                 <center>ERP</center> CARSON
-            </h3>
+            </h3> -->
         </a>
         <!--end::Logo-->
     </div>
